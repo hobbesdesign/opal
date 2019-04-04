e6a03810c2e357af8875616cff0ee77b67a62c8b
Lucas Vocos <lucas.vocos@me.com>
2019-04-04 12:56:35 -0400

    forEach() function on the lottieFiles array

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const lottieFiles = [{ name: "Cart_Build", id: "icon-1"},{ name: "Edit_Build", id: "icon-2"},{ name: "Folder_Build", id: "icon-3"},{ name: "Garbage_Build", id: "icon-4"},{ name: "Heart_Build", id: "icon-5"},{ name: "Home_Build", id: "icon-6"},{ name: "Laptop_Build", id: "icon-7"},{ name: "Printer_Build", id: "icon-8"},{ name: "Redo_Build", id: "icon-9"},{ name: "Repeat_Build", id: "icon-10"},{ name: "Cart_Build copy", id: "icon-11"},{ name: "Edit_Build copy", id: "icon-12"},{ name: "Folder_Build copy", id: "icon-13"},{ name: "Garbage_Build copy", id: "icon-14"},{ name: "Heart_Build copy", id: "icon-15"},{ name: "Home_Build copy", id: "icon-16"},{ name: "Laptop_Build copy", id: "icon-17"},{ name: "Printer_Build copy", id: "icon-18"},{ name: "Redo_Build copy", id: "icon-19"},{ name: "Repeat_Build copy", id: "icon-20"},{ name: "Cart_Build copy 2", id: "icon-21"},{ name: "Edit_Build copy 2", id: "icon-22"},{ name: "Folder_Build copy 2", id: "icon-23"},{ name: "Garbage_Build copy 2", id: "icon-24"},{ name: "Heart_Build copy 2", id: "icon-25"},{ name: "Home_Build copy 2", id: "icon-26"},{ name: "Laptop_Build copy 2", id: "icon-27"},{ name: "Printer_Build copy 2", id: "icon-28"},{ name: "Redo_Build copy 2", id: "icon-29"},{ name: "Repeat_Build copy 2", id: "icon-30"},{ name: "Cart_Build copy 3", id: "icon-31"},{ name: "Edit_Build copy 3", id: "icon-32"},{ name: "Folder_Build copy 3", id: "icon-33"},{ name: "Garbage_Build copy 3", id: "icon-34"},{ name: "Heart_Build copy 3", id: "icon-35"},{ name: "Home_Build copy 3", id: "icon-36"},{ name: "Laptop_Build copy 3", id: "icon-37"},{ name: "Printer_Build copy 3", id: "icon-38"},{ name: "Redo_Build copy 3", id: "icon-39"},{ name: "Repeat_Build copy 3", id: "icon-40"},{ name: "Cart_Build copy 4", id: "icon-41"},{ name: "Edit_Build copy 4", id: "icon-42"},{ name: "Folder_Build copy 4", id: "icon-43"},{ name: "Garbage_Build copy 4", id: "icon-44"},{ name: "Heart_Build copy 4", id: "icon-45"},{ name: "Home_Build copy 4", id: "icon-46"},{ name: "Laptop_Build copy 4", id: "icon-47"},{ name: "Printer_Build copy 4", id: "icon-48"},{ name: "Redo_Build copy 4", id: "icon-49"},{ name: "Repeat_Build copy 4", id: "icon-50"},{ name: "Cart_Build copy 5", id: "icon-51"},{ name: "Edit_Build copy 5", id: "icon-52"},{ name: "Folder_Build copy 5", id: "icon-53"},{ name: "Garbage_Build copy 5", id: "icon-54"},{ name: "Heart_Build copy 5", id: "icon-55"},{ name: "Home_Build copy 5", id: "icon-56"},{ name: "Laptop_Build copy 5", id: "icon-57"},{ name: "Printer_Build copy 5", id: "icon-58"},{ name: "Redo_Build copy 5", id: "icon-59"},{ name: "Repeat_Build copy 5", id: "icon-60"},{ name: "Cart_Build copy 6", id: "icon-61"},{ name: "Edit_Build copy 6", id: "icon-62"},{ name: "Folder_Build copy 6", id: "icon-63"},{ name: "Garbage_Build copy 6", id: "icon-64"},{ name: "Heart_Build copy 6", id: "icon-65"},{ name: "Home_Build copy 6", id: "icon-66"},{ name: "Laptop_Build copy 6", id: "icon-67"},{ name: "Printer_Build copy 6", id: "icon-68"},{ name: "Redo_Build copy 6", id: "icon-69"},{ name: "Repeat_Build copy 6", id: "icon-70"},{ name: "Cart_Build copy 7", id: "icon-71"},{ name: "Edit_Build copy 7", id: "icon-72"},{ name: "Folder_Build copy 7", id: "icon-73"},{ name: "Garbage_Build copy 7", id: "icon-74"},{ name: "Heart_Build copy 7", id: "icon-75"},{ name: "Home_Build copy 7", id: "icon-76"},{ name: "Laptop_Build copy 7", id: "icon-77"},{ name: "Printer_Build copy 7", id: "icon-78"},{ name: "Redo_Build copy 7", id: "icon-79"},{ name: "Repeat_Build copy 7", id: "icon-80"},{ name: "Cart_Build copy 8", id: "icon-81"},{ name: "Edit_Build copy 8", id: "icon-82"},{ name: "Folder_Build copy 8", id: "icon-83"},{ name: "Garbage_Build copy 8", id: "icon-84"},{ name: "Heart_Build copy 8", id: "icon-85"},{ name: "Home_Build copy 8", id: "icon-86"},{ name: "Laptop_Build copy 8", id: "icon-87"},{ name: "Printer_Build copy 8", id: "icon-88"},{ name: "Redo_Build copy 8", id: "icon-89"},{ name: "Repeat_Build copy 8", id: "icon-90"},{ name: "Cart_Build copy 9", id: "icon-91"},{ name: "Edit_Build copy 9", id: "icon-92"},{ name: "Folder_Build copy 9", id: "icon-93"},{ name: "Garbage_Build copy 9", id: "icon-94"},{ name: "Heart_Build copy 9", id: "icon-95"},{ name: "Home_Build copy 9", id: "icon-96"},{ name: "Laptop_Build copy 9", id: "icon-97"},{ name: "Printer_Build copy 9", id: "icon-98"},{ name: "Redo_Build copy 9", id: "icon-99"},{ name: "Repeat_Build copy 9", id: "icon-100"}]
 const gridJson = {name: "Test_Grid", id: "grid-container"}
+const browserJSON = [];
 
 function playFile(file) {
   let animation = lottie.loadAnimation({
@@ -10,11 +11,18 @@ function playFile(file) {
     autoplay: true,
     name: `${file.name} Animation`,
   });
-
-
 }
+
 function setName(file) {
   $(`#${file.id}`).attr(`data-name`, `${file.name}`);
+}
+
+function createJSON(file) {
+  $.getJSON(`json/${file.name}.json`, function(obj) {
+    console.log(obj);
+  });
+  browserJSON.push(obj);
+  debugger;
 }
 
 function adjustTop() {
@@ -98,7 +106,6 @@ function setColor() {
 
   // $lottieColor will be applied to the new JSON files
 
-
   function hslToHex(h,s,l) {
     s /= 100;
     l /= 100;
@@ -167,16 +174,14 @@ $('input').on('input', function() {
 
 $(document).ready(function() {
 
-
-  // lottieFiles.forEach(playFile);
   lottieFiles.forEach(setName);
+  lottieFiles.forEach(createJSON);
   playFile(gridJson)
   // add a .then(adjustTop();)
 
   $(".icon").click(function(event) {
 
-    let iconID = event.currentTarget.id,
-    jsonName = event.currentTarget.attributes[2].value;
+    let iconID = event.currentTarget.id;
 
     // Get data-name value from the object I click
     // pass that value into the path name, instead of looping through the objects
@@ -195,7 +200,7 @@ $(document).ready(function() {
       }
 
       $.getJSON(`json/${div.name}.json`, function(obj) {
-        // console.log(obj);
+        console.log(obj);
         // https://github.com/sonaye/lottie-editor/blob/dev/src/components/Root.js#L119-L124
 
       });
