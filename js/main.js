1123cb92ca9ed3f634e81d29587d8c53324c6388
Lucas Vocos <lucas.vocos@me.com>
2019-04-05 13:50:10 -0400

    function to get framerate

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const lottieFiles = [{ name: "Cart_Build", id: "icon-1"},{ name: "Edit_Build", id: "icon-2"},{ name: "Folder_Build", id: "icon-3"},{ name: "Garbage_Build", id: "icon-4"},{ name: "Heart_Build", id: "icon-5"},{ name: "Home_Build", id: "icon-6"},{ name: "Laptop_Build", id: "icon-7"},{ name: "Printer_Build", id: "icon-8"},{ name: "Redo_Build", id: "icon-9"},{ name: "Repeat_Build", id: "icon-10"},{ name: "Cart_Build copy", id: "icon-11"},{ name: "Edit_Build copy", id: "icon-12"},{ name: "Folder_Build copy", id: "icon-13"},{ name: "Garbage_Build copy", id: "icon-14"},{ name: "Heart_Build copy", id: "icon-15"},{ name: "Home_Build copy", id: "icon-16"},{ name: "Laptop_Build copy", id: "icon-17"},{ name: "Printer_Build copy", id: "icon-18"},{ name: "Redo_Build copy", id: "icon-19"},{ name: "Repeat_Build copy", id: "icon-20"},{ name: "Cart_Build copy 2", id: "icon-21"},{ name: "Edit_Build copy 2", id: "icon-22"},{ name: "Folder_Build copy 2", id: "icon-23"},{ name: "Garbage_Build copy 2", id: "icon-24"},{ name: "Heart_Build copy 2", id: "icon-25"},{ name: "Home_Build copy 2", id: "icon-26"},{ name: "Laptop_Build copy 2", id: "icon-27"},{ name: "Printer_Build copy 2", id: "icon-28"},{ name: "Redo_Build copy 2", id: "icon-29"},{ name: "Repeat_Build copy 2", id: "icon-30"},{ name: "Cart_Build copy 3", id: "icon-31"},{ name: "Edit_Build copy 3", id: "icon-32"},{ name: "Folder_Build copy 3", id: "icon-33"},{ name: "Garbage_Build copy 3", id: "icon-34"},{ name: "Heart_Build copy 3", id: "icon-35"},{ name: "Home_Build copy 3", id: "icon-36"},{ name: "Laptop_Build copy 3", id: "icon-37"},{ name: "Printer_Build copy 3", id: "icon-38"},{ name: "Redo_Build copy 3", id: "icon-39"},{ name: "Repeat_Build copy 3", id: "icon-40"},{ name: "Cart_Build copy 4", id: "icon-41"},{ name: "Edit_Build copy 4", id: "icon-42"},{ name: "Folder_Build copy 4", id: "icon-43"},{ name: "Garbage_Build copy 4", id: "icon-44"},{ name: "Heart_Build copy 4", id: "icon-45"},{ name: "Home_Build copy 4", id: "icon-46"},{ name: "Laptop_Build copy 4", id: "icon-47"},{ name: "Printer_Build copy 4", id: "icon-48"},{ name: "Redo_Build copy 4", id: "icon-49"},{ name: "Repeat_Build copy 4", id: "icon-50"},{ name: "Cart_Build copy 5", id: "icon-51"},{ name: "Edit_Build copy 5", id: "icon-52"},{ name: "Folder_Build copy 5", id: "icon-53"},{ name: "Garbage_Build copy 5", id: "icon-54"},{ name: "Heart_Build copy 5", id: "icon-55"},{ name: "Home_Build copy 5", id: "icon-56"},{ name: "Laptop_Build copy 5", id: "icon-57"},{ name: "Printer_Build copy 5", id: "icon-58"},{ name: "Redo_Build copy 5", id: "icon-59"},{ name: "Repeat_Build copy 5", id: "icon-60"},{ name: "Cart_Build copy 6", id: "icon-61"},{ name: "Edit_Build copy 6", id: "icon-62"},{ name: "Folder_Build copy 6", id: "icon-63"},{ name: "Garbage_Build copy 6", id: "icon-64"},{ name: "Heart_Build copy 6", id: "icon-65"},{ name: "Home_Build copy 6", id: "icon-66"},{ name: "Laptop_Build copy 6", id: "icon-67"},{ name: "Printer_Build copy 6", id: "icon-68"},{ name: "Redo_Build copy 6", id: "icon-69"},{ name: "Repeat_Build copy 6", id: "icon-70"},{ name: "Cart_Build copy 7", id: "icon-71"},{ name: "Edit_Build copy 7", id: "icon-72"},{ name: "Folder_Build copy 7", id: "icon-73"},{ name: "Garbage_Build copy 7", id: "icon-74"},{ name: "Heart_Build copy 7", id: "icon-75"},{ name: "Home_Build copy 7", id: "icon-76"},{ name: "Laptop_Build copy 7", id: "icon-77"},{ name: "Printer_Build copy 7", id: "icon-78"},{ name: "Redo_Build copy 7", id: "icon-79"},{ name: "Repeat_Build copy 7", id: "icon-80"},{ name: "Cart_Build copy 8", id: "icon-81"},{ name: "Edit_Build copy 8", id: "icon-82"},{ name: "Folder_Build copy 8", id: "icon-83"},{ name: "Garbage_Build copy 8", id: "icon-84"},{ name: "Heart_Build copy 8", id: "icon-85"},{ name: "Home_Build copy 8", id: "icon-86"},{ name: "Laptop_Build copy 8", id: "icon-87"},{ name: "Printer_Build copy 8", id: "icon-88"},{ name: "Redo_Build copy 8", id: "icon-89"},{ name: "Repeat_Build copy 8", id: "icon-90"},{ name: "Cart_Build copy 9", id: "icon-91"},{ name: "Edit_Build copy 9", id: "icon-92"},{ name: "Folder_Build copy 9", id: "icon-93"},{ name: "Garbage_Build copy 9", id: "icon-94"},{ name: "Heart_Build copy 9", id: "icon-95"},{ name: "Home_Build copy 9", id: "icon-96"},{ name: "Laptop_Build copy 9", id: "icon-97"},{ name: "Printer_Build copy 9", id: "icon-98"},{ name: "Redo_Build copy 9", id: "icon-99"},{ name: "Repeat_Build copy 9", id: "icon-100"}]
-const gridJson = {name: "Test_Grid", id: "grid-container"}
+const gridJson = {name: "Test_Grid", id: "grid-container"};
 const browserJSON = [];
 
 function playFile(file) {
@@ -18,28 +18,28 @@ function setName(file) {
 }
 
 function createJSON(file) {
+  // could also just use a map() instead of a forEach and it will include pushing itinto browserJSON
+  // const browserJSON = lottieFiles.map(createJSON);
   $.getJSON(`json/${file.name}.json`, function(obj) {
-    // console.log(obj);
     browserJSON.push(obj);
+    return obj;
   });
-  return browserJSON;
+
 }
 
-console.log(browserJSON);
 
 function editJSON(file, lottieColor, scaleStrokeDuration) {
-    // get color, stroke, scale, duration values from setColor() and setScaleStrokeDuration()
-    debugger;
-
-    let $duration,
-    $height,
+    file.op = 30;
+    let $height,
     $width,
-    $color = lottieColor;
+    $color = lottieColor,
+    $scale = scaleStrokeDuration[0],
+    $stroke = scaleStrokeDuration[1],
+    $duration = scaleStrokeDuration[2].replace("ms",""),
+    $framerate = (file.op/$duration)*1000;
 
-    // scaleStrokeDuration is an array, break down those values and apply height, width,
-    // and do some calculation for the duration to turn it into frames per second
 
-    // file.fr = $duration;
+    file.fr = $framerate.toFixed(2);
     // file.h = $height;
     // file.w = $width;
 
@@ -127,8 +127,6 @@ function setColor() {
 
   }
 
-  // $lottieColor will be applied to the new JSON files
-
   function hslToHex(h,s,l) {
     s /= 100;
     l /= 100;
@@ -169,9 +167,9 @@ function setColor() {
     return "#" + r + g + b;
   }
 
-  let $colorrgba = hslToRgb($hue,$saturation,$lightness),
-  $lottieColor = rgbatolottie($hue, $saturation, $lightness),
-  $colorhex = hslToHex($hue,$saturation,$lightness),
+  let $lottieColor = rgbatolottie($hue, $saturation, $lightness),
+  // $colorrgba = hslToRgb($hue,$saturation,$lightness),
+  // $colorhex = hslToHex($hue,$saturation,$lightness),
   $fill = $(".fill"),
   $stroke = $(".stroke"),
   $saturationBar = $("#visibleSaturation"),
@@ -186,8 +184,6 @@ function setColor() {
   $lightnessBar.css(`background`, `linear-gradient(to right, hsl(${$hue}, 100%, 0%), hsl(${$hue}, 100%, 20%), hsl(${$hue}, 100%, 30%), hsl(${$hue}, 100%, 40%), hsl(${$hue}, 100%, 50%), hsl(${$hue}, 100%, 60%), hsl(${$hue}, 100%, 70%), hsl(${$hue}, 100%, 80%),hsl(${$hue}, 100%, 90%), hsl(${$hue}, 100%, 100%))`);
 
   return $lottieColor;
-
-
 
 }
 
@@ -214,14 +210,11 @@ $(document).ready(function() {
   lottieFiles.forEach(setName);
   lottieFiles.forEach(createJSON);
   playFile(gridJson)
-  // add a .then(adjustTop();)
 
   $(".icon").click(function(event) {
 
     let iconID = event.currentTarget.id;
 
-    // Get data-name value from the object I click
-    // pass that value into the path name, instead of looping through the objects
     function playFilePreview(div) {
       if (div.id == iconID) {
         $("#preview").empty();
@@ -236,14 +229,8 @@ $(document).ready(function() {
         })
       }
 
-      $.getJSON(`json/${div.name}.json`, function(obj) {
-        console.log(obj);
-        // https://github.com/sonaye/lottie-editor/blob/dev/src/components/Root.js#L119-L124
-
-      });
       // Return a promise here, will allow .then(setColor()) function to run after and apply itself to the svg
     }
-    // lottieFiles.find(playFilePreview).then(setColor());
     lottieFiles.find(playFilePreview);
   })
 
